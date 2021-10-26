09884e682693ba45937968e524a89bb6658a61fe
Alba <alba.lp95@gmail.com>
2021-10-26 02:23:05 -0500

    First exercises

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+
+// let system = os.platform();
+// let name = os.hostname();
+fs.appendFile('prueba-texto.txt', (error) => {
+    if (error)  throw error;
+   console.log('Success!!')  
+});
+//console.log(cpu);
